8b332a7c7643aff90faf2661bf8e5e2f3a964bd7
Shannon <shannonj112@gmail.com>
2019-06-15 21:26:52 -0400

    Search function performs GET correctly

--- app.py
+++ app.py
@@ -2,18 +2,19 @@ from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
-
+z
 @app.route('/')
 def home():
 	return render_template('index.html')
 
 @app.route('/search', methods=['GET'])
 def search():
-	print("ha")
 	if request.method == 'GET':
-		PARAMS= request.args.getlist('search[]')
-		print('hi')
+		#print("hi")
+		PARAMS= request.args.getlist('q')
 		print(PARAMS)
+
+
 	return render_template('search.html')
 
 
